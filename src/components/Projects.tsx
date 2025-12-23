27a54705c2a8a4f6c984fa5988c3f59782de2ae8
Levin Kooss <levinjpn@gmail.com>
2025-12-23 16:25:55 +1300

    Add links to dev projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -17,22 +17,43 @@ export const Projects = () => {
       
       <div id="projects" className="flex flex-col gap-6 w-full max-w-[1280px]">
         
-      <div className="w-full flex justify-center mb-2">
+        <div className="w-full flex justify-center mb-2">
            <h2 className="text-[#A1A1AA]/60 font-mono text-xs tracking-[0.2em] uppercase">
              Selected Work
            </h2>
-      </div>
+        </div>
 
-      <div className="relative flex flex-col md:flex-row lg:flex-row gap-6">
-        <PulseLogCard />
-        <CaptuneCard />
-      </div>
+        {/* ROW 1: DEV PROJECTS (NOW CLICKABLE) */}
+        <div className="relative flex flex-col md:flex-row lg:flex-row gap-6">
+          
+          {/* Link to PulseLog Code */}
+          <a 
+            href="https://pulselog-gray.vercel.app" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="flex-1 block"
+          >
+            <PulseLogCard />
+          </a>
+
+          {/* Link to Captune Code */}
+          <a 
+            href="https://captune.vercel.app"
+            target="_blank" 
+            rel="noopener noreferrer" 
+            className="flex-1 block"
+          >
+            <CaptuneCard />
+          </a>
+
+        </div>
         
-      <div className="relative flex flex-col lg:flex-row gap-6 justify-between">
-        <LumiCard />
-        <LexiCard />
-        <FitHubCard />
-      </div>
+        {/* ROW 2: CASE STUDIES (INTERNAL) */}
+        <div className="relative flex flex-col lg:flex-row gap-6 justify-between">
+          <LumiCard />
+          <LexiCard />
+          <FitHubCard />
+        </div>
 
       </div>
     </section>
